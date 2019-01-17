0f5766d36930dbaa94c9071f3a1233bc768f6532
周子龙 <zhouzilong@zhouzilongdemacbook-pro.local>
2019-01-17 20:37:47 +0800

    Initial Commit

---
+++ Leecode/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Leecode
+//
+//  Created by  周子龙 on 2019/1/17.
+//  Copyright © 2019  周子龙. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
