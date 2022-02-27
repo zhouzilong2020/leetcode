2b09ee9923d1e889c7ed3e7bdeb91d0de8a28f40
Zilong Zhou <529620861@qq.com>
2022-02-27 11:05:20 +0800

    created Binary Tree

---
+++ 2022-02-08/tag/tree/Binary Search Tree/230_Kth_Smallest_Element_in_a_BST/solution_test.go
@@ -0,0 +1,25 @@
+package kthsmallestelementinabst
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func traverse(root *TreeNode, k int) int {
+	if root == nil {
+		return -1
+	}
+
+	traverse(root.Left, k)
+	k -= 1
+	if k == 0 {
+		return root.Val
+	}
+	traverse(root.Right, k)
+}
+
+func kthSmallest(root *TreeNode, k int) int {
+
+	return traverse(root, k)
+}
