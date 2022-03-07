6dc1c87f5ece79ae99588ee364fbe1c80517e1fc
Zilong Zhou <529620861@qq.com>
2022-03-07 21:52:20 +0800

    341

---
+++ 2022-02-08/tag/tree/other/341_Flatten_Nested_List_Iterator/solution_test.go
@@ -0,0 +1,58 @@
+package flattennestedlistiterator
+
+// This is the interface that allows for creating nested lists.
+// You should not implement it, or speculate about its implementation
+type NestedInteger struct {
+}
+
+// Return true if this NestedInteger holds a single integer, rather than a nested list.
+func (this NestedInteger) IsInteger() bool {}
+
+// Return the single integer that this NestedInteger holds, if it holds a single integer
+// The result is undefined if this NestedInteger holds a nested list
+// So before calling this method, you should have a check
+func (this NestedInteger) GetInteger() int {}
+
+// Set this NestedInteger to hold a single integer.
+func (n *NestedInteger) SetInteger(value int) {}
+
+// Set this NestedInteger to hold a nested list and adds a nested integer to it.
+func (this *NestedInteger) Add(elem NestedInteger) {}
+
+// Return the nested list that this NestedInteger holds, if it holds a nested list
+// The list length is zero if this NestedInteger holds a single integer
+// You can access NestedInteger's List element directly if you want to modify it
+func (this NestedInteger) GetList() []*NestedInteger {}
+
+type NestedIterator struct {
+	list []*NestedInteger
+}
+
+func Constructor(nestedList []*NestedInteger) *NestedIterator {
+	return &NestedIterator{list: nestedList}
+}
+
+func (this *NestedIterator) Next() int {
+	temp := this.list[0]
+	this.list = this.list[1:]
+	return temp.GetInteger()
+}
+
+func (this *NestedIterator) HasNext() bool {
+
+	for len(this.list) != 0 && !this.list[0].IsInteger() {
+		first := this.list[0].GetList()
+		remainedList := this.list[1:]
+
+		var expandedList []*NestedInteger
+		for _, ele := range first {
+			expandedList = append(expandedList, ele)
+		}
+		for _, remainedEle := range remainedList {
+			expandedList = append(expandedList, remainedEle)
+		}
+		this.list = expandedList
+	}
+
+	return len(this.list) != 0
+}
